55c282359dbe489885ec45212bf07a4e9ecf3874
tuvantran225 <tuvantran225@gmail.com>
2019-10-28 23:53:00 +0700

    Init

--- jquery-templates/index.js
+++ jquery-templates/index.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     var employeeData = [
-        { FirstName: "Rob", LastName: "Mathews", Age: 26 },
-        { FirstName: "Richie", LastName: "Richards", Age: 32 },
-        { FirstName: "Shawn", LastName: "Clarke", Age: 53 },
-        { FirstName: "Dave", LastName: "Canterburry", Age: 42 }
+        { firstName: "Rob", lastName: "Mathews", age: 27 },
+        { firstName: "Richie", lastName: "Richards", age: 26 },
+        { firstName: "Shawn", lastName: "Clarke", age: 26 }
     ];
 
-    $("#myTemplate").tmpl(employeeData).appendTo("#employeeContainer");
+    $("#myTemplate").tmpl(employeeData).appendTo("tbody");
 });
